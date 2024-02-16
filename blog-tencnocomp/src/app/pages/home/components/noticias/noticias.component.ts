da7421f3aba0a7601c2ac5d17ed07e19cea0990f
HenriqueGalvim <henrixe80@gmail.com>
2024-02-16 16:22:35 -0400

    Feita as alterações no componente de noticias

--- blog-tencnocomp/src/app/pages/home/components/noticias/noticias.component.ts
+++ blog-tencnocomp/src/app/pages/home/components/noticias/noticias.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./noticias.component.css']
 })
 export class NoticiasComponent {
-
+  noticias: any = [
+    {
+      titulo:'Primeira Noticia',
+      autor:'Ryan',
+      noticia:'Lorem ipsum sksjsjkasssasajlslasskkaklsakls'
+    },
+    {
+      titulo:'Segunda Noticia',
+      autor:'Kauê',
+      noticia:'Lorem ipsum 124791cuqpt32anxworppx,wew'
+    },
+    {
+      titulo:'Terceira Noticia',
+      autor:'Gabriel',
+      noticia:'Lorem ipsum 120r69729axowhoybfeij'
+    },
+    {
+      titulo:'Quarta Noticia',
+      autor:'Henrique',
+      noticia:'Lorem ipsum awh3utg2oy3g3r023gn323tu8'
+    }
+  ]
 }
